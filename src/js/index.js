1296ea288bfe8f9a2015e48dcbc890cd1952c0ce
Vitoria Floriano <viefloriano@hotmail.com>
2023-11-25 16:44:28 -0300

    adicionado novo projeto Previsao do Tempo

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+const chaveDaApi = "57ec5294f94744f3b09112206232311";
+const botaoDeBusca = document.querySelector(".btn-busca");
+
+botaoDeBusca.addEventListener("click", async () => {
+    const cidade = document.getElementById("input-busca").value;
+    if (!cidade) return;
+    const dados = await buscarDadosDaCidade(cidade);
+    if (dados) preencherDadosNaTela(dados, cidade);
+});
+
+async function buscarDadosDaCidade(cidade) {
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
+    const resposta = await fetch(apiUrl);
+    if (resposta.status !== 200) return;
+    const dados = resposta.json();
+    return dados;
+}
+
+function preencherDadosNaTela(dados, cidade) {
+    const temperatura = dados.current.temp_c;
+    const condicao = dados.current.condition.text;
+    const umidade = dados.current.humidity;
+    const velocidadeDoVento = dados.current.wind_kph;
+    const iconeCondicao = dados.current.condition.icon;
+    document.getElementById("cidade").textContent = cidade;
+    document.getElementById("temperatura").textContent = `${temperatura} °C`;
+    document.getElementById("condicao").textContent = condicao;
+    document.getElementById("umidade").textContent = `${umidade}%`;
+    document.getElementById("velocidade-do-vento").textContent = `${velocidadeDoVento}km/h`;
+    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
+}
